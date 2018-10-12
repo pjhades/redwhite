ef34c30398b85115864efe61895b0858dd689452
Jing Peng <pj.hades@gmail.com>
2018-10-12 14:36:59 -0400

    ptdump: Add SDL2 window display

--- ptdump/src/main.rs
+++ ptdump/src/main.rs
@@ -1,10 +1,16 @@
 extern crate redwhite;
+extern crate sdl2;
 
-use redwhite::{Tile, TILESIZE, ines, error};
-use error::{Error, ResultContext};
+use redwhite::{Tile, TILESIZE};
+use redwhite::error::{Error, ResultContext};
+use redwhite::ines::Ines;
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 
-fn run() -> Result<(), Error> {
-    let rom = ines::Ines::from_file("/Users/jing.peng/Downloads/super-mario-bro.nes")?;
+const NTILE: u32 = 16;
+
+fn merge_tiles(rom: &Ines) -> Vec<Tile> {
     let mut tiles: Vec<Tile> = Vec::with_capacity(rom.chrrom.len() / 16);
 
     for chunk in rom.chrrom.chunks(16) {
@@ -18,7 +24,39 @@ fn run() -> Result<(), Error> {
         tiles.push(tile);
     }
 
+    tiles
+}
+
+fn dump(tiles: &Vec<Tile>) -> Result<(), Error> {
+    let sdl_ctx = sdl2::init().map_err(|s| Error::new(s))?;
+    let video = sdl_ctx.video().map_err(|s| Error::new(s))?;
+
+    let pixel_height = NTILE * TILESIZE as u32;
+    let pixel_width = pixel_height * 2;
+
+    let window = video.window("ptdump", pixel_width, pixel_height).position_centered().build()?;
+    let mut canvas = window.into_canvas().build()?;
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_ctx.event_pump().unwrap();
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
+                _ => {}
+            }
+        }
+    }
+
     Ok(())
+}
+
+fn run() -> Result<(), Error> {
+    let rom = Ines::from_file("/Users/jing.peng/Downloads/super-mario-bro.nes")?;
+    let tiles = merge_tiles(&rom);
+    dump(&tiles)
 }
 
 fn main() {
