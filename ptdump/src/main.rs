88267802301dae730b0fe620740710066d27c47c
Jing Peng <pj.hades@gmail.com>
2018-10-12 13:45:55 -0400

    ptdump: Store each tile with [u32;8]
    So with [[u8;8];8] we need 64 bytes per tile but with [u32;8] we
    only need 32 bytes.

--- ptdump/src/main.rs
+++ ptdump/src/main.rs
@@ -1,9 +1,7 @@
 extern crate redwhite;
 
-use redwhite::{TILESIZE, ines, error};
+use redwhite::{Tile, TILESIZE, ines, error};
 use error::{Error, ResultContext};
-
-type Tile = [[u8;TILESIZE];TILESIZE];
 
 fn run() -> Result<(), Error> {
     let rom = ines::Ines::from_file("/Users/jing.peng/Downloads/super-mario-bro.nes")?;
@@ -11,9 +9,10 @@ fn run() -> Result<(), Error> {
 
     for chunk in rom.chrrom.chunks(16) {
         let mut tile = Tile::default();
-        for i in (0..TILESIZE) {
-            for j in (0..TILESIZE) {
-                tile[i][TILESIZE - j - 1] = ((((chunk[i + 8] >> j)) & 0x1) << 1) | ((chunk[i] >> j) & 0x1);
+        for i in 0..TILESIZE {
+            for j in 0..TILESIZE {
+                tile[i] |= (((((chunk[i + TILESIZE] >> j) & 1) << 1) |
+                           ((chunk[i] >> j) & 1)) as u32) << (j * 4);
             }
         }
         tiles.push(tile);
