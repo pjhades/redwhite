aec2925aebf107d53a46a5b02dab7bc0efdbaf4f
Jing Peng <pj.hades@gmail.com>
2018-10-12 16:21:33 -0400

    ptdump: Fix coordinate computation error

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
+extern crate sdl2;
+
 pub mod cpu;
 pub mod mem;
 pub mod ines;
 pub mod error;
+pub mod palette;
 
+pub const PIXEL_UNIT: usize = 4;
 // Each tile is a 8x8 grid of pixels.
 pub const TILESIZE: usize = 8;
-
 // The color of each pixel is described with 4 bits.
 pub type Tile = [u32;TILESIZE];
---
+++ src/palette.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+use std::path::Path;
+use std::io::Read;
+use error::Error;
+use sdl2::pixels::Color;
+
+const NCOLOR: usize = 16;
+const NPALETTE: usize = 4;
+const PALETTE_SIZE: usize = 48;
+
+pub type Palette = [Color;NCOLOR];
+pub type PaletteSet = [Palette;NPALETTE];
+
+pub fn palette_from_file<P: AsRef<Path>>(path: P) -> Result<PaletteSet, Error> {
+    let mut file = File::open(path)?;
+    let mut pal = [[Color::RGB(255, 255, 255); NCOLOR]; NPALETTE];
+
+    for i in 0..NPALETTE {
+        let mut bytes = [0u8;PALETTE_SIZE];
+        file.read_exact(&mut bytes)?;
+        for (j, chunk) in bytes.chunks(3).enumerate() {
+            pal[i][j] = Color::RGB(chunk[0], chunk[1], chunk[2]);
+        }
+    }
+
+    Ok(pal)
+}
