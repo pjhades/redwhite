9b7ad1dc783d97690e4f344d430cc242ff19863f
Jing Peng <pj.hades@gmail.com>
2018-10-12 13:45:55 -0400

    ptdump: Store each tile with [u32;8]
    So with [[u8;8];8] we need 64 bytes per tile but with [u32;8] we
    only need 32 bytes.

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,8 @@ pub mod mem;
 pub mod ines;
 pub mod error;
 
+// Each tile is a 8x8 grid of pixels.
 pub const TILESIZE: usize = 8;
+
+// The color of each pixel is described with 4 bits.
+pub type Tile = [u32;TILESIZE];
