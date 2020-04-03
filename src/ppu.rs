8260a8d09d6be78da864c9ee6ad1ae5dc9514a39
Jing Peng <pj.hades@gmail.com>
2020-04-03 17:23:47 +0800

    Read/Write PPU registers

--- src/ppu.rs
+++ src/ppu.rs
@@ -1,10 +1,23 @@
 use mem::Access;
 
+const PPUCTRL:   u16 = 0x2000;
+const PPUMASK:   u16 = 0x2001;
+const PPUSTATUS: u16 = 0x2002;
+const OAMADDR:   u16 = 0x2003;
+const OAMDATA:   u16 = 0x2004;
+const PPUSCROLL: u16 = 0x2005;
+const PPUADDR:   u16 = 0x2006;
+const PPUDATA:   u16 = 0x2007;
+const OAMDMA:    u16 = 0x4014;
+
 pub struct Ppu {
-    vaddr: u16, // current VRAM address
-    taddr: u16, // temporary VRAM address
-    finex: u8, // fine X scroll
+    vaddr: u16,         // current VRAM address
+    taddr: u16,         // temporary VRAM address
+    finex: u8,          // fine X scroll
     write_toggle: bool, // PPUSCROLL, PPUADDR write toggle
+    // http://wiki.nesdev.com/w/index.php/PPU_registers
+    regs: [u8; 8],      // registers
+    data_bus: u8,        // internal data bus/latch
 }
 
 impl Ppu {
@@ -16,14 +29,21 @@ impl Ppu {
             taddr: 0,
             finex: 0,
             write_toggle: false,
+            regs: [0; 8],  // TODO are you sure?
+            data_bus: 0,
         }
     }
 
     pub fn read_reg(&self, addr: u16) -> u8 {
-        0x23
+        if addr == PPUSTATUS || addr == OAMDATA || addr == PPUDATA {
+            self.data_bus = self.regs[(addr - 0x2000) as usize];
+        }
+        self.data_bus
     }
 
     pub fn write_reg(&mut self, addr: u16, value: u8) {
+        self.data_db = value;
+        self.regs[(addr - 0x2000) as usize] = value;
     }
 }
 
